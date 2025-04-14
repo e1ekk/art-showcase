5824b9bc29972b023a4e029078c782640bb6a9da
e1ekk <47przemek@gmail.com>
2025-04-14 19:25:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function seten(){
 }
 
 var setlanguage = localStorage.getItem("language");
-var languages = JSON.parse(`{"en":[{"fullscreen":"Open in fullscreen", "en":"Change language to English", "pl":"Change language to Polish", "featured":"Featured", "allart":"All art", "desc1":"My very first creation which started the whole thing. The inspiration came from <a href='https://www.reddit.com/r/ForzaHorizon/comments/1attra8/d_b_s/' target='_blank'>this</a> Reddit post. Off to a good start.", "desc5":"Made as a request from a friend. I swear, it wasn't my idea.", "desc6":"Got inspired by <a href='https://www.reddit.com/r/ForzaHorizon/comments/1fvyhpw/classic/' target='_blank'>this</a> Reddit post.", "desc8":"The map is <a href='https://www.youtube.com/watch?v=OJbvLW8ICUo' target='_blank'>Hoshido City</a>. There's also a refreshed version which released 4 days later called <a href='https://www.youtube.com/watch?v=9iz3FcQ1zyE' target='_blank'>Hoshido City Redux</a>.", "desc9":"Made for Halloween.", "desc10":"Took the photos with a random guy driving around the race track."}], "pl":[{"fullscreen":"Otwórz w widoku pełnoekranowym", "en":"Zmień język na angielski", "pl":"Zmień język na polski", "featured":"Wyróżnione", "allart":"Wszystkie prace", "desc1":"Moja pierwsza kreacja, od której to wszystko się zaczęło. Inspiracja wzięła się z <a href='https://www.reddit.com/r/ForzaHorizon/comments/1attra8/d_b_s/' target='_blank'>tego</a> posta na Reddicie. Na dobry początek.", "desc5":"Zrobione na prośbę znajomej. Przysięgam, to nie był mój pomysł.", "desc6":"Zainspirowałem się <a href='https://www.reddit.com/r/ForzaHorizon/comments/1fvyhpw/classic/' target='_blank'>tym</a> postem na Reddicie.", "desc8":"Mapa nazywa się <a href='https://www.youtube.com/watch?v=OJbvLW8ICUo' target='_blank'>Hoshido City</a>. Istnieje też odświeżona wersja, która została wydana 4 dni później, a nazywa się <a href='https://www.youtube.com/watch?v=9iz3FcQ1zyE' target='_blank'>Hoshido City Redux</a>.", "desc9":"Zrobione z okazji Halloween.", "desc10":"Zdjęcia zrobiłem z losową osobą, która jeździła wokół toru."}]}`);
+var languages = JSON.parse(`{"en":[{"fullscreen":"Open in fullscreen", "en":"Change language to English", "pl":"Change language to Polish", "featured":"Featured", "allart":"All art", "desc1":"My very first creation which started the whole thing. The inspiration came from <a href='https://www.reddit.com/r/ForzaHorizon/comments/1attra8/d_b_s/' target='_blank'>this</a> Reddit post. Off to a good start.", "desc5":"Got inspired by <a href='https://www.reddit.com/r/ForzaHorizon/comments/1fvyhpw/classic/' target='_blank'>this</a> Reddit post.", "desc7":"The map is <a href='https://www.youtube.com/watch?v=OJbvLW8ICUo' target='_blank'>Hoshido City</a>. There's also a refreshed version which released 4 days later called <a href='https://www.youtube.com/watch?v=9iz3FcQ1zyE' target='_blank'>Hoshido City Redux</a>.", "desc8":"Made for Halloween.", "desc9":"Took the photos with a random guy driving around the race track."}], "pl":[{"fullscreen":"Otwórz w widoku pełnoekranowym", "en":"Zmień język na angielski", "pl":"Zmień język na polski", "featured":"Wyróżnione", "allart":"Wszystkie prace", "desc1":"Moja pierwsza kreacja, od której to wszystko się zaczęło. Inspiracja wzięła się z <a href='https://www.reddit.com/r/ForzaHorizon/comments/1attra8/d_b_s/' target='_blank'>tego</a> posta na Reddicie. Na dobry początek.", "desc5":"Zainspirowałem się <a href='https://www.reddit.com/r/ForzaHorizon/comments/1fvyhpw/classic/' target='_blank'>tym</a> postem na Reddicie.", "desc7":"Mapa nazywa się <a href='https://www.youtube.com/watch?v=OJbvLW8ICUo' target='_blank'>Hoshido City</a>. Istnieje też odświeżona wersja, która została wydana 4 dni później, a nazywa się <a href='https://www.youtube.com/watch?v=9iz3FcQ1zyE' target='_blank'>Hoshido City Redux</a>.", "desc8":"Zrobione z okazji Halloween.", "desc9":"Zdjęcia zrobiłem z losową osobą, która jeździła wokół toru."}]}`);
 if(setlanguage == "en"){
     var fullscreen = languages.en[0].fullscreen;
-    for(var i=14; i>0; i--) document.getElementsByName("fullscreenjson")[i].title=fullscreen;
+    for(var i=13; i>0; i--) document.getElementsByName("fullscreenjson")[i].title=fullscreen;
     var en = languages.en[0].en;
     document.getElementById("enjson").title=en;
     var pl = languages.en[0].pl;
@@ -45,18 +45,16 @@ if(setlanguage == "en"){
     document.getElementById("desc1json").innerHTML=desc1;
     var desc5 = languages.en[0].desc5;
     document.getElementById("desc5json").innerHTML=desc5;
-    var desc6 = languages.en[0].desc6;
-    document.getElementById("desc6json").innerHTML=desc6;
+    var desc7 = languages.en[0].desc7;
+    document.getElementById("desc7json").innerHTML=desc7;
     var desc8 = languages.en[0].desc8;
     document.getElementById("desc8json").innerHTML=desc8;
     var desc9 = languages.en[0].desc9;
     document.getElementById("desc9json").innerHTML=desc9;
-    var desc10 = languages.en[0].desc10;
-    document.getElementById("desc10json").innerHTML=desc10;
 }
 if(setlanguage == "pl"){
     var fullscreen = languages.pl[0].fullscreen;
-    for(var i=14; i>=0; i--) document.getElementsByName("fullscreenjson")[i].title=fullscreen;
+    for(var i=13; i>=0; i--) document.getElementsByName("fullscreenjson")[i].title=fullscreen;
     var en = languages.pl[0].en;
     document.getElementById("enjson").title=en;
     var pl = languages.pl[0].pl;
@@ -69,12 +67,10 @@ if(setlanguage == "pl"){
     document.getElementById("desc1json").innerHTML=desc1;
     var desc5 = languages.pl[0].desc5;
     document.getElementById("desc5json").innerHTML=desc5;
-    var desc6 = languages.pl[0].desc6;
-    document.getElementById("desc6json").innerHTML=desc6;
+    var desc7 = languages.pl[0].desc7;
+    document.getElementById("desc7json").innerHTML=desc7;
     var desc8 = languages.pl[0].desc8;
     document.getElementById("desc8json").innerHTML=desc8;
     var desc9 = languages.pl[0].desc9;
     document.getElementById("desc9json").innerHTML=desc9;
-    var desc10 = languages.pl[0].desc10;
-    document.getElementById("desc10json").innerHTML=desc10;
 }
