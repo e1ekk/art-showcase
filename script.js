4eeb4a1d901ffd54c72c9974a97abaf9e266c630
e1ekk <47przemek@gmail.com>
2025-04-21 14:01:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-const scroll = 150;
-window.addEventListener("scroll", () => {
-    const currentScroll = window.scrollY;
-    if(currentScroll <= scroll){
-        var opacity = 1 - currentScroll / scroll;
-        var height = 150 - 150 * currentScroll / scroll;
-        var marginleft = -75 + 75 * currentScroll / scroll;
-    }else{
-        opacity = 0;
-        height = 0;
-    }
-    var logo = document.getElementById("logo").style;
-    logo.opacity = opacity;
-    logo.height = height;
-    logo.marginLeft = marginleft;
-});
-
 var languagevalue;
 
 function setpl(){
